1dc1fede9855d8409bfec681343c263cf6c5930c
Alejandro Barria <alesergio972@gmail.com>
2023-03-20 21:09:30 -0300

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('.darkmode').addEventListener("click", invert);
+function invert(){
+   if( document.querySelector('#pagecontainer').className= "invert"){
+    document.querySelector('#pagecontainer').className= "";
+   } else {
+
+  
+    document.querySelector('#pagecontainer').className= "invert";
+}
+}
